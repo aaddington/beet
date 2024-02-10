c36317648116f93b4ff51a222aa98c6322679b17
Alex Addington <axaddington@uga.edu>
2024-02-10 15:52:16 -0500

    implemented footer styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,14 +2,35 @@ import '../css/header.css'
 
 function Header() {
     return (
+
         <div className="header">
-            <a href="#">Home</a>
-            <div className="dropdown">
-                <a href="#">Dropdown</a>
-                <div className="dropdown-content">
-                    <a href="#">Option 1</a>
-                    <a href="#">Option 2</a>
-                    <a href="#">Option 3</a>
+            <div className={'left'}>
+                <a href="#">Home</a>
+            </div>
+            <div className={'right'}>
+                <div className="dropdown">
+                    <a href="#">Solutions</a>
+                    <div className="dropdown-content">
+                         <a href="#">Option 1</a>
+                        <a href="#">Option 2</a>
+                        <a href="#">Option 3</a>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <a href="#">Help</a>
+                    <div className="dropdown-content">
+                        <a href="#">Option 1</a>
+                        <a href="#">Option 2</a>
+                        <a href="#">Option 3</a>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <a href="#">Partners</a>
+                    <div className="dropdown-content">
+                        <a href="#">Option 1</a>
+                        <a href="#">Option 2</a>
+                        <a href="#">Option 3</a>
+                    </div>
                 </div>
             </div>
         </div>
