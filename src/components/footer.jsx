a9ca6474e4d8484e05162d09fe5a9272ea79c372
Alex Addington <axaddington@uga.edu>
2024-02-10 15:12:26 -0500

    implemented some stuff

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,42 @@ import '../css/footer.css'
 function Footer() {
     return (
         <div className={'footer'}>
-            <a href={"http://google.com"}>©beet</a>
+            <div className={'wrap'}>
+                <div className={'col'}>
+                    <h2>Jerry</h2>
+                    <ul>
+                        <li>Solution</li>
+                        <li>Ideas</li>
+                    </ul>
+                </div>
+                <div className={'col'}>
+                    <h2>Jerry</h2>
+                    <ul>
+                        <li>How it works</li>
+                        <li>Previous Accomplishments</li>
+                    </ul>
+                </div>
+                <div className={'col'}>
+                    <h2>Jerry</h2>
+                    <ul>
+                        <li>Jerry</li>
+                        <li>Lewis</li>
+                    </ul>
+                </div>
+                <div className={'col'}>
+                    <h2>Jerry</h2>
+                    <ul>
+                        <li>Jerry</li>
+                        <li>Lewis</li>
+                    </ul>
+                </div>
+            </div>
+            <p>
+                © 2023 Bookit. All Rights Reserved. All movies on this platform
+                are trademarks of, and all related images and content are the
+                property of, Bookit and the people who worked on it. Duplication
+                and copy of this is strictly prohibited. All rights reserved.
+            </p>
         </div>
     );
 }
