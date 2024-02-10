6abb011b16a908b9dc3e31f62ee13782deac1d7d
Alex Addington <axaddington@uga.edu>
2024-02-10 15:12:26 -0500

    implemented some stuff

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,17 @@ import '../css/header.css'
 
 function Header() {
     return (
-        <nav className={'header'}>
-            <a href={"http://google.com"}>beet</a>
-        </nav>
+        <div className="header">
+            <a href="#">Home</a>
+            <div className="dropdown">
+                <a href="#">Dropdown</a>
+                <div className="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                    <a href="#">Option 3</a>
+                </div>
+            </div>
+        </div>
     );
 }
 
