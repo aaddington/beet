038103685ef63a0310e62e49c46189b07f8abe41
Alex Addington <axaddington@uga.edu>
2024-02-11 01:06:11 -0500

    implemented Home much better

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,37 +1,15 @@
 import '../css/header.css'
-
+import icon from '../assets/images/beet.png'
 function Header() {
     return (
-
         <div className="header">
             <div className={'left'}>
-                <a href="#">Home</a>
+                <a href="/"><img id="img_logo" src={icon} alt={'Logo'}/></a>
             </div>
             <div className={'right'}>
-                <div className="dropdown">
                     <a href="#">Solutions</a>
-                    <div className="dropdown-content">
-                        <a href="#">Option 1</a>
-                        <a href="#">Option 2</a>
-                        <a href="#">Option 3</a>
-                    </div>
-                </div>
-                <div className="dropdown">
                     <a href="#">Help</a>
-                    <div className="dropdown-content">
-                        <a href="#">Option 1</a>
-                        <a href="#">Option 2</a>
-                        <a href="#">Option 3</a>
-                    </div>
-                </div>
-                <div className="dropdown">
                     <a href="#">Partners</a>
-                    <div className="dropdown-content">
-                        <a href="#">Option 1</a>
-                        <a href="#">Option 2</a>
-                        <a href="#">Option 3</a>
-                    </div>
-                </div>
             </div>
         </div>
     );
